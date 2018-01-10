6248c39c5d1b592779d7a42bdbf909169a31e5dd
Simeon Walker <s.walker@bangor.ac.uk>
2018-01-10 12:47:35 +0000

    PEP8 fixes

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@
 # See also LICENSE.txt
 
 """adpasswd.py Pure Python Command line interface to change Active Directory Passwords via LDAP.
-	usage: adpasswd.py username [password]
+    usage: adpasswd.py username [password]
 """
 
-import os.path
-import glob
+# import os.path
+# import glob
 
 from distutils.core import setup
-
 
 longdesc = open("README.txt").read()
 
@@ -20,26 +19,27 @@ classifiers = [
     "Environment :: Console",
     "Intended Audience :: Developers",
     "Intended Audience :: System Administrators",
-	"Intended Audience :: Information Technology",
-	"License :: OSI Approved :: GNU General Public License (GPL)",
+    "Intended Audience :: Information Technology",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-	"Topic :: System :: Systems Administration",
-	"Topic :: System :: Systems Administration :: Authentication/Directory",
-	"Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
-	"Topic :: Utilities",
+    "Topic :: System :: Systems Administration",
+    "Topic :: System :: Systems Administration :: Authentication/Directory",
+    "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+    "Topic :: Utilities",
     ]
 
-setup(name="adpasswd",
-      version="0.21",
-      description=__doc__.strip(),
-      long_description=longdesc,
-      keywords="ldap activedirectory AD",
-      classifiers=classifiers,
-      author="Craig Sawyer",
-      author_email="csawyer@yumaed.org",
-      url="https://launchpad.net/adpasswd",
-      license="GPL v2",
-	  py_modules=['ldaplib'],
-	  scripts=['adpasswd.py']
-      )
+setup(
+    name="adpasswd",
+    version="0.21",
+    description=__doc__.strip(),
+    long_description=longdesc,
+    keywords="ldap activedirectory AD",
+    classifiers=classifiers,
+    author="Craig Sawyer",
+    author_email="csawyer@yumaed.org",
+    url="https://launchpad.net/adpasswd",
+    license="GPL v2",
+    py_modules=['ldaplib'],
+    scripts=['adpasswd.py']
+)
